5f7b3de95e291275bfa766dce50f922d2d904498
arun071 <arun07160@gmail.com>
2024-06-07 11:29:45 +0530

    Product added

--- src/Product.java
+++ src/Product.java
@@ -16,35 +16,43 @@ public class Product {
         this.productName = productName;
     }
 
-    public static void addProduct(Product p, ArrayList<Product> productList) {
-
-        productList.add(p);
-    }
-
     public static void editProduct() {
     }
 
     public static void deleteProduct() {
     }
 
-    public static void updateProduct() {
+    public void updatecount(int count) {
+        this.availableCount = count;
+
     }
 
-    public static void viewProduct() {
+    public void viewProduct() {
+        System.out.println("Product Name: " + productName);
+        System.out.println("Category: " + category);
+        System.out.println("Price: " + price);
+        System.out.println("Available Count: " + availableCount);
     }
 
     public static void availableProduct() {
-    }
 
+    }
+}
+
+class MyClass {
     public static void main(String[] args) {
         ArrayList<Product> productList = new ArrayList<>();
-
-
-        Product p1 = new Product(100, 10, "Food", "Biscuits");
+        ArrayList<ArrayList<Product>> productdb = new ArrayList<>();
+        Product p1 = new Product(100, 10, "Food", "Soap");
         Product p2 = new Product(100, 10, "Food", "Chocolate");
-        Product p3 = new Product(100, 10, "Food", "Soap");
-        addProduct(p1, productList);
-        addProduct(p2, productList);
-        addProduct(p3, productList);
+        Product p3 = new Product(100, 10, "Food", "Biscuits");
+
+        p1.viewProduct();
+        System.out.println("");
+        p1.updatecount(2);
+        System.out.println("");
+        p1.viewProduct();
+
+
     }
 }
